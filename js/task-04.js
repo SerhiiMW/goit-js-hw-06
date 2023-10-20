5f1535dad5160cbb2511b8fd155e96d2374d6027
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-20 16:35:32 +0300

    task4

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+
+buttonDecrement.addEventListener("click", () => {
+    counterValue -= 1
+   return document.querySelector("#value").innerHTML = counterValue;
+});
+
+buttonIncrement.addEventListener("click", () => {
+    counterValue += 1
+    return document.querySelector("#value").innerHTML = counterValue;
+});
