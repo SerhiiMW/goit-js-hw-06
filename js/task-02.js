478db4167ea1e88c12ccbdd3bc4ef944e4624ac5
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-20 12:40:03 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients")
+
+ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  list.appendChild(liEl)
+  })
+
