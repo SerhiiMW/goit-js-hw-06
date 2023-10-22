9b08a85e25c42c3d4c13e055c298729b168c8cdb
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-22 16:09:37 +0300

    task7

---
+++ js/task-07.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("#font-size-control")
+const textEl = document.querySelector("#text")
+
+let length = 0;
+length = inputEl.value.length
+
+// inputEl.addEventListener("input", (event) => {
+//     let length = 0;
+//     length = event.currentTarget.value.length
+//         // = textEl.style.fontSize
+//     }); 
+
+
+console.log(length)
