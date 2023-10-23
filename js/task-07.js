b71e4f5578b10c6d2f28363b86b010e10e13a56f
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-23 17:43:40 +0300

    task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,20 +1,10 @@
 const inputEl = document.querySelector("#font-size-control")
 const textEl = document.querySelector("#text")
-const text = textEl.firstChild
 
+let startFontSizeNumber = inputEl.valueAsNumber;
 
-
-    // let length = 0;
-    // length += 1
-    // textEl.style.fontSize = length
-   
-    for (let i = 0; i <= 96; i += 1) {
-        inputEl.addEventListener("input", (event) => {
-        let length = event.currentTarget.value.trim().length
-        length += i
+inputEl.addEventListener("input", () => {
+    startFontSizeNumber = inputEl.valueAsNumber;
+    textEl.style.fontSize = startFontSizeNumber + "px";  
     })
-    console.log(length)
-    }; 
-
-
-// console.log(inputEl.previousSibling)
+ 
