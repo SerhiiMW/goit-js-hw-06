cee064fdb2bb2e0e2c0499cb4af49d4c1ad75e00
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-23 13:14:40 +0300

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -2,17 +2,13 @@ const elInput = document.querySelector("input")
 const inputLength = document.querySelector('input[data-length="6"]');
 
 
-// const inputValid = document.querySelector("style #validation-input.valid #borderColor")
-// const inputValid = document.style
-
 elInput.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.trim().length === Number(inputLength.dataset.length)
+    if (event.currentTarget.value.trim().length !== Number(inputLength.dataset.length)
     ) {
-        elInput.style.borderColor = "#4caf50";
+        elInput.classList.add("invalid");
     } else {
-        elInput.style.borderColor = "#f44336"
+        elInput.classList.replace("invalid", "valid");
     }
-    }); 
+    });
 
-// console.log(inputValid)
-// console.dir(inputValid)
+
