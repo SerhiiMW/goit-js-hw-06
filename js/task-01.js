932a898e990edfa948b883c1e5dedc9527774092
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-25 11:24:51 +0300

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const ulItem = document.querySelectorAll(".item")
 console.log(`Number of categories: ${ulItem.length}`)
 
 ulItem.forEach((element, i) => {
-    const titleLi = element.querySelector("h2");
+    const titleLi = element.firstElementChild;
     console.log("");
     console.dir(titleLi.textContent);
-    const ulItemLi = element.querySelectorAll("li");
+    const ulItemLi = element.lastElementChild.children;
     console.dir(`Element: ${ulItemLi.length}`)
 })
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,23 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreate = document.querySelector('button[data-create]')
+const btnDestroy = document.querySelector('button[data-destroy]')
+const boxes = document.querySelector("#boxes")
+const inputEl = document.querySelector("input")
+
+btnCreate.addEventListener("click", createBoxes)
+function createBoxes(amount) {
+  const divEl = document.createElement("div");
+  // amount = inputEl.currentTarget.value
+  divEl.style.width = "30 px";
+  divEl.style.height = "30 px";
+  divEl.style.color = getRandomHexColor;
+  boxes.append(divEl)
+}
+
+btnCreate.addEventListener("click", destroyBoxes)
+function destroyBoxes() {
+  boxes.firstChild.remove();
+}
+// console.log(btnDestroy)
